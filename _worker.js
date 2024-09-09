30e3cc69fa71cd7f31726359acdc2a5e461508e5
handsomwht686868 <18298470325@139.com>
2024-09-09 21:31:37 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -844,7 +844,7 @@ const generateRemark = (index, port) => {
     switch (index) {
         case 0:
         case 1:
-            remark = `💦 BPB - Domain_${index + 1} : ${port}`;
+            remark = `💦 BPB - ruihang_${index + 1} : ${port}`;
             break;
         case 2:
         case 3:
